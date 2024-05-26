1ed26dc1c661b4ed1096b2429bc3423bc4398e00
Jer <144748914+rjeremypaul@users.noreply.github.com>
2024-05-27 07:16:35 +0800

    additions
    I tried to create a registration function inside the code but i am having difficulties to make it functionable

---
+++ menus.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-dark bg-dark bg-gradient navbar-expand-md my-5 flex- ms-3" style="padding-bottom: 80%" >
+
+    <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
+        <button class="navbar-toggler align-self-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="nav navbar-nav flex-column menus" style="font-size: 20px;">
+                <li class="nav-item"><a class="nav-link" href="index.php" id="index_menu">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="category.php" id="category_menu">Category</a></li>
+                <li class="nav-item"><a class="nav-link" href="brand.php" id="brand_menu">Brand</a></li>
+                <li class="nav-item"><a class="nav-link" href="supplier.php" id="supplier_menu">Supplier</a></li>
+                <li class="nav-item"><a class="nav-link" href="product.php" id="product_menu">Product</a></li>
+                <li class="nav-item"><a class="nav-link" href="purchase.php" id="purchase_menu">Purchase</a></li>
+                <li class="nav-item"><a class="nav-link" href="order.php" id="order_menu">Orders</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
